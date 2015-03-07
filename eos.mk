6bfde47f906ec27965d15234e88ba59df70af1ca
777jon <jcruzdeveloper@gmail.com>
2015-03-07 07:19:22 -0500

    d851: Euphorize
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- eos.mk
+++ eos.mk
@@ -14,15 +14,26 @@
 # limitations under the License.
 #
 
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from d851 device
-$(call inherit-product, device/lge/d851/device.mk)
+$(call inherit-product, device/lge/d851/d851.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d851
-PRODUCT_NAME := full_d851
+PRODUCT_NAME := eos_d851
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D851
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
